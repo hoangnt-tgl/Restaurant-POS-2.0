306894f40b7fce709163b4ee85d5a2ad0baa7670
thesamurai3011 <thesamurai3011@gmail.com>
2021-06-20 10:19:56 +0700

    fix footer

--- OrderManager/om.php
+++ OrderManager/om.php
@@ -224,10 +224,90 @@ foreach ($orderList as $od){
             </div>
         </div>
     </div>
+    <!-- Phần footer -->
     <!-- Footer -->
     <footer class="text-center text-lg-start bg-light text-muted">
         
-        
+        <!-- Section: Links  -->
+        <section class="p-1">
+        <div class="container text-left text-md-start mt-1">
+            <!-- Grid row -->
+            <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <!-- Content -->
+                <h4 class="text-uppercase fw-bold mb-4">
+                <i class="fas fa-utensils"></i>Nova Restaurant
+                </h4>
+                <p>
+                We are the best choice for you
+                
+                </p>
+            </div>
+            <!-- Grid column -->
+    
+            <!-- Grid column -->
+            <!-- <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            
+                <h6 class="text-uppercase fw-bold mb-4">
+                Products
+                </h6>
+                <p>
+                <a href="#!" class="text-reset">Angular</a>
+                </p>
+                <p>
+                <a href="#!" class="text-reset">React</a>
+                </p>
+                <p>
+                <a href="#!" class="text-reset">Vue</a>
+                </p>
+                <p>
+                <a href="#!" class="text-reset">Laravel</a>
+                </p>
+            </div> -->
+            <!-- Grid column -->
+    
+            <!-- Grid column -->
+            <!-- <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            
+                <h6 class="text-uppercase fw-bold mb-4">
+                Useful links
+                </h6>
+                <p>
+                <a href="#!" class="text-reset">Pricing</a>
+                </p>
+                <p>
+                <a href="#!" class="text-reset">Settings</a>
+                </p>
+                <p>
+                <a href="#!" class="text-reset">Orders</a>
+                </p>
+                <p>
+                <a href="#!" class="text-reset">Help</a>
+                </p>
+            </div> -->
+            <!-- Grid column -->
+    
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <!-- Links -->
+                <h6 class="text-uppercase fw-bold mb-4">
+                Contact
+                </h6>
+                <p><i class="fas fa-home me-3"></i> Đại Học Bách Khoa TP.HCM</p>
+                <p>
+                <i class="fas fa-envelope me-3"></i>
+                novateam@gmail.com
+                </p>
+                <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+            </div>
+            <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+        </section>
+        <!-- Section: Links  -->
     
         <!-- Copyright -->
         <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
